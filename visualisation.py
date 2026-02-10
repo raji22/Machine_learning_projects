9d412e268d6c1c951c44ecd101917de322743205
raji22 <radieshster@gmail.com>
2026-02-10 19:39:49 +0530

    Delete visualisation.py

--- visualisation.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("world_top_restaurants_dataset.csv")
-print(df.head())
-
-columns = df.columns
-print(columns)
-star_category_unique = df['Star_Category'].unique()
-print(star_category_unique)
